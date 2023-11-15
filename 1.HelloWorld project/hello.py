2dc3158413b2b95c91bae0982612747ad887c031
NakibingeSteven <88508645+NakibingeSteven@users.noreply.github.com>
2023-11-15 15:41:08 +0300

    Writing  HelloWorld Code

--- 1.HelloWorld project/hello.py
+++ 1.HelloWorld project/hello.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World"
+    return "Hello World Nakibinge Steven "
 
 
 if __name__ == "__main":
-    app.reun(debug=True)
+    app.run(debug=True)
