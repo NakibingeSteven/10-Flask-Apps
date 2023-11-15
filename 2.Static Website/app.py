7fc0ebd42bf50bd37b66ecd4a3a0c46f39570e32
NakibingeSteven <88508645+NakibingeSteven@users.noreply.github.com>
2023-11-15 17:23:07 +0300

    Updating home into indx route

--- 2.Static Website/app.py
+++ 2.Static Website/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 
+app = Flask(__name__)
 
-@app.route("/home")
+
+@app.route("/")
 def home():
     return render_template("home.html")
 
