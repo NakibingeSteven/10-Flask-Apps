c1513399b685b3ee0eddb48715bbdba60221092a
NakibingeSteven <88508645+NakibingeSteven@users.noreply.github.com>
2023-11-15 16:40:58 +0300

    Static Website Code

---
+++ 2.Static Website/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main":
+    app.run(debug=True)
