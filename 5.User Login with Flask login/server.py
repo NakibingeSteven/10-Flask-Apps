c320126a6e794822a10794805a4b9e6b376f7aff
NakibingeSteven <88508645+NakibingeSteven@users.noreply.github.com>
2023-11-19 17:32:42 +0300

    Refactoring teh whole code

--- 5.User Login with Flask login/server.py
+++ 5.User Login with Flask login/server.py
@@ -7,32 +7,24 @@ from flask_login import (
     logout_user,
     current_user,
 )
-from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "mysecretkey"  # Set a secret key for session management
-login_manager = LoginManager(app)  # LoginManager is initialized with the Flask app
-
-# login_view attribute is set to the endpoint ('login') where the user should be redirected
-# if they are not logged in and try to access a protected resource.
-login_manager.login_view = "login"
+app.config["SECRET_KEY"] = "mysecretkey"
+login_manager = LoginManager(app)
 
 
-# Mock user class (replace this with your User model and database logic)
 class User(UserMixin):
     def __init__(self, user_id, username, email, password, age, sex, address):
         self.id = user_id
         self.username = username
         self.email = email
-        self.password = (
-            password  # In a real application, store hashed passwords, not plain text.
-        )
+        self.password = generate_password_hash(password)
         self.age = age
         self.sex = sex
         self.address = address
 
 
-# Mock user database (replace this with your database logic)
 users = {
     "1": User(
         "1",
@@ -56,7 +48,6 @@ users = {
 }
 
 
-# this loads a user
 @login_manager.user_loader
 def load_user(user_id):
     return users.get(user_id)
@@ -64,7 +55,7 @@ def load_user(user_id):
 
 @app.route("/")
 def home():
-    return "<h1>This is the Home Page<?h1>"
+    return "<h1>This is the Home Page</h1>"
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -72,9 +63,11 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+
         user = next(
             (user for user in users.values() if user.username == username), None
         )
+
         if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for("dashboard"))
@@ -82,9 +75,25 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return (
+        f'Hello, {current_user.id}! This is the dashboard. <a href="/logout">Logout</a>'
+    )
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        # getting the values from the form
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
@@ -105,23 +114,9 @@ def register():
         login_user(new_user)
 
         return redirect(url_for("dashboard"))
+
+    # render teh template
     return render_template("register.html")
-
-
-# protected using login_required
-@app.route("/dashboard")
-@login_required
-def dashboard():
-    return (
-        f'Hello, {current_user.id}! This is the dashboard. <a href="/logout">Logout</a>'
-    )
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
